44ca1bea6325b9d542cca11898bfc3bf2e309ef0
rodrigo <rodrigomacielth@gmail.com>
2022-09-18 11:31:52 -0300

    Cambios de efectos

--- js/navbar.js
+++ js/navbar.js
@@ -6,49 +6,13 @@ const about = document.querySelector('.about')
 
 
 window.addEventListener('scroll',()=>{
-    header.classList.toggle('abajo', window.scrollY>0);
-})
-
-window.addEventListener('scroll',()=>{
-    const scrolled = window.scrollY;
-    const val = scrolled*0.8;
-    const ancho1 = window.innerWidth;
-
-    if(val<488 && ancho1> 500){
-        skil.style.transform = `translateX(${1*val}%)`;
-    }
-    if(val == 488 && ancho1> 500){
-        skil.style.transform = `translateX(488%)`;
-    }
-    if(val<200 && ancho1< 500){
-        skil.style.transform = `translateX(${1*val}%)`;
-    }
-    if(val == 488 && ancho1< 500){
-        skil.style.transform = `translateX(150%)`;
-    }
+    scrol = parseInt(window.scrollY*0.1);
+    nav.classList.toggle('abajo', window.scrollY>50)
+    nav.classList.toggle('abajo1', window.scrollY<25)
+    console.log(parseInt(window.scrollY*0.2))
 })
 
 
-window.addEventListener('scroll',()=>{
-    const scrolled = window.scrollY;
-    const ancho = window.innerWidth
-    const val = (scrolled*0.7)-488;
-    const val2 = (scrolled*0.22)-488;
-    if(scrolled>500 && scrolled<1100 && ancho> 500){
-        about.style.transform = `translateX(${1*val}%)`;
-        
-    }
-    if(scrolled == 500 && ancho> 500){
-        about.style.transform = `translateX(488%)`;
-    }
-    if(scrolled>2500 && ancho< 500){
-        about.style.transform = `translateX(${(val2)}%)`;
-        console.log(scrolled)
-    }
-    if(scrolled>3100 && ancho< 500){
-        about.style.transform = `translateX(200%)`;
-    }
-})
 
 
 
